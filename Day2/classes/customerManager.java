9314a7f38b8c2b7b76fdcb32e159c9db1c8ecb9e
Gizem Duman <45661081+dumanngizem@users.noreply.github.com>
2021-04-27 13:00:38 +0300

    Add files via upload

---
+++ Day2/classes/customerManager.java
@@ -0,0 +1,15 @@
+package classes;
+
+public class customerManager {
+
+	public static void Add(){
+		System.out.println("Müþteri Eklendi.");
+	}
+	public static void Remove(){
+		System.out.println("Müþteri Kaldýrýldý.");
+	}
+	public static void Update(){
+		System.out.println("Müþteri Güncellendi.");
+	}
+
+}
---
+++ Day2/classes/main.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		customerManager  customermanager = new customerManager();
+		customerManager.Add();
+		customerManager.Remove();
+		customerManager.Update();
+		
+	}
+
+}
