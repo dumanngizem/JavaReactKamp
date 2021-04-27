1417adf412cc91e57ab53217fa34bdb5dfb366b0
Gizem Duman <45661081+dumanngizem@users.noreply.github.com>
2021-04-27 12:14:02 +0300

    Sesli Harfler

---
+++ Day1/sesliHarfler.java
@@ -0,0 +1,24 @@
+
+public class sesliHarfler {
+
+	public static void main(String[] args) {
+		
+		char harf = 'A';
+
+		switch(harf) {
+			
+			case 'A': 
+			case 'I': 
+			case 'O': 
+			case 'U': 
+				
+				System.out.println("Kalýn Sesli Harf");
+				break;
+				
+			default:
+				System.out.println("Ýnce Sesli Harf");
+		}
+
+	}
+
+}
