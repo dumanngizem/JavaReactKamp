aaa4e9303af5868505d47d4c5ecf4e4eb6e81145
Gizem Duman <45661081+dumanngizem@users.noreply.github.com>
2021-04-29 00:44:33 +0300

    3.Ders

---
+++ inheritance/IndividualCustomer.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class IndividualCustomer extends Customer{
+	
+	//extend geniþletmek demektir ýndividual customerýn geniþletilmiþ hali
+	String firstName;
+	String lastName;
+	String nationalIdentity;
+
+}
+
